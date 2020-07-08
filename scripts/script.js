026616842570e84b5e98385c8d1fd262a32591d1
ylxsbn <64317771+ylxsbn@users.noreply.github.com>
2020-07-08 16:13:56 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,16 @@
 function openNav() {
-  document.getElementById("Sidebar").style.width = "250px";
-  document.getElementById("video_bg").style.marginLeft = "250px";
-  document.getElementById("Link").style.opacity = "1";
+  document.getElementById("Sidebar").style.width = "37vh";
+  document.getElementById("video_bg").style.marginLeft = "37vh";
   document.getElementById("Logo").style.opacity = "1";
-  document.getElementById("opBtn").style.display = "none";
-  document.getElementById("clBtn").style.display = "block";
+  document.getElementById("list").style.opacity = "1";
+  document.getElementById("opBtn").style.transform = "translateX(-30vh)";
+  document.getElementById("clBtn").style.transform = "translateX(37vh)";
 }
 
 function closeNav() {
   document.getElementById("Sidebar").style.width = "0px";
   document.getElementById("video_bg").style.marginLeft = "0px";
-  document.getElementById("Link").style.opacity = "0";
-  document.getElementById("Logo").style.opacity = "0";
-  document.getElementById("clBtn").style.display = "none";
-  document.getElementById("opBtn").style.display = "block";    
-}
+	document.getElementById("opBtn").style.transform = "translateX(0vh)";
+
+document.getElementById("clBtn").style.transform = "translateX(0vh)";
+  }
